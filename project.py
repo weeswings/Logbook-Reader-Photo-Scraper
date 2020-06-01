7f3943077e931316b83b446299a4d5665bb27b07
Adam Clark <63400384+Adamcjc01@users.noreply.github.com>
2020-06-01 12:03:48 +0100

    Add files via upload
    Uploading the main project file

---
+++ project.py
@@ -0,0 +1,45 @@
+from bs4 import BeautifulSoup
+import urllib
+import requests
+from PIL import Image
+from io import BytesIO
+import pandas as pd
+
+def define_URL(date,reg):
+	#Create and return the URL with the Date and Registration inserted.
+	return "https://www.airliners.net/search?datePhotographed={}&registrationActual={}&sortBy=dateAccepted&sortOrder=desc&perPage=36&display=detail""".format(date,reg)
+
+def get_request(date,reg):
+	try:
+		url = define_URL(date,reg)
+		headers = {}
+		headers['User-Agent'] = "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"
+		req = urllib.request.Request(url, headers = headers)
+		resp = urllib.request.urlopen(req)
+		respData = resp.read()
+		return respData
+	except Exception as e:
+	    print(str(e))
+
+
+def get_photo_info(date,reg):
+	try:
+		Data = get_request(date,reg)
+		soup = BeautifulSoup(Data, 'html.parser')
+
+		div=soup.find('div', class_="ps-v2-results-photo")
+		print("--------\nwww.airliners.net{}".format(div.find('a').get('href')))
+		photo = div.find('img')
+		photo_src =photo.get('src')
+		response = requests.get(photo_src)
+		img = Image.open(BytesIO(response.content))
+		img.show()
+	except AttributeError:
+		print("No photograph found for {} on date: {}".format(reg,date))
+
+if __name__ == '__main__':
+	data = pd.read_csv("Logbook.csv")
+	data['Date'] = pd.to_datetime(data['Date'])
+
+	for iterator in range(0,len(data)):
+		get_photo_info(data.loc[iterator,'Date'].strftime('%Y-%m-%d'),data.loc[iterator, 'Registration'])
